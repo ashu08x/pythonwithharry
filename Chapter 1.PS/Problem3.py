59296336bd745e451e7dd9df56d3d6c01bf69c08
ashu08x <ashulab08x>
2025-10-30 05:05:18 +0530

    Chapter1.PS modified

--- Chapter 1.PS/Problem3.py
+++ Chapter 1.PS/Problem3.py
@@ -4,5 +4,5 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("Hello, Amish Raj")
+engine.say("Hello, Sheru Raj")
 engine.runAndWait()
